0df4b8ebe70bcba3f5627c429e6df562cf1b2a9f
Heidi Erwin <heidianneerwin@gmail.com>
2019-10-12 15:30:58 -0400

    jousting scene

---
+++ RomanceLance/Assets/Scripts/Joust.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Joust : MonoBehaviour
+{
+    [SerializeField] Image intenseEyes;
+    [SerializeField] GameObject player;
+    [SerializeField] GameObject npc;
+
+    private float runTime = 2.5f; // how long it takes the characters to run across the screen
+
+    public bool charactersMoving = false;
+
+    private void Start() {
+         StartJoust();
+    }
+
+    public void StartJoust() {
+        StartCoroutine(RunFirstHalf());
+    }
+
+    IEnumerator RunFirstHalf() {
+        StartCoroutine(MoveToPosition(npc.transform, new Vector3(0, .5f, 0), runTime));
+        StartCoroutine(MoveToPosition(player.transform, new Vector3(0, -.5f, 0), runTime));
+        StartCoroutine(WaitThenRunSecondHalf());
+        yield return null;
+    }
+
+    private void RunSecondHalf() {
+        StartCoroutine(MoveToPosition(npc.transform, new Vector3(-8, .5f, 0), runTime));
+        StartCoroutine(MoveToPosition(player.transform, new Vector3(8, -.5f, 0), runTime));
+    }
+
+    IEnumerator WaitThenRunSecondHalf() {
+        yield return new WaitForSeconds(2.5f);
+        ShowIntenseEyes();
+        yield return new WaitForSeconds(1f);
+        HideIntenseEyes();
+        RunSecondHalf();
+    }
+
+    public void ShowIntenseEyes() {
+        intenseEyes.gameObject.SetActive(true);
+    }
+
+    public void HideIntenseEyes() {
+        intenseEyes.gameObject.SetActive(false);
+    }
+
+    public IEnumerator MoveToPosition(Transform transform, Vector3 position, float timeToMove) {
+        Vector3 currentPos = transform.position;
+        float t = 0f;
+        while (t < 1) {
+            t += Time.deltaTime / timeToMove;
+            transform.position = Vector3.Lerp(currentPos, position, t);
+            yield return null;
+        }
+    }
+}
---
+++ RomanceLance/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+    public void BackToMenu() {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void StartGame() {
+        SceneManager.LoadScene("Dialogue");
+    }
+
+    public void ShowCredits() {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ShowInstructions() {
+        SceneManager.LoadScene("Instructions");
+    }
+}
