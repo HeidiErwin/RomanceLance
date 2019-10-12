b50552c6cc06888b532008edb2c678a8e6f6f206
sa-ge <sagematsushima@gmail.com>
2019-10-12 14:52:06 -0400

    Squidward shirts

---
+++ RomanceLance/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] Image shirt;
+    [SerializeField] Sprite blackShirt;
+    [SerializeField] Sprite whiteShirt;
+
+    public void changeShirt(int index)
+    {
+            if (index == 0)
+        {
+            shirt.sprite = blackShirt;
+        }
+        else
+        {
+            shirt.sprite = whiteShirt;
+        }
+    }
+}
