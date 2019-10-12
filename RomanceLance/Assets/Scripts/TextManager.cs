1933a1b05da4339bf1f6cf4ef4a56012aadd54f9
David Smith <dms92@protonmail.ch>
2019-10-12 15:30:08 -0400

    added dialogue placeholder

---
+++ RomanceLance/Assets/Scripts/TextManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public Text txt;
+    int textIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            textIndex++;
+            txt.text = textIndex.ToString();
+        }
+    }
+}
