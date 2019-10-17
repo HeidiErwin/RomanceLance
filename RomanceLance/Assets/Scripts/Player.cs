ac5dce161ac76c23f98ec04512c8955a6134d9de
sa-ge <sagematsushima@gmail.com>
2019-10-16 23:34:59 -0400

    created buttons to change the shirts

--- RomanceLance/Assets/Scripts/Player.cs
+++ RomanceLance/Assets/Scripts/Player.cs
@@ -8,6 +8,10 @@ public class Player : MonoBehaviour
     [SerializeField] Image shirt;
     [SerializeField] Sprite blackShirt;
     [SerializeField] Sprite whiteShirt;
+    [SerializeField] Sprite redShirt;
+    [SerializeField] Sprite orangeShirt;
+    [SerializeField] Sprite yellowShirt;
+    [SerializeField] Sprite purpleShirt;
 
     public void changeShirt(int index)
     {
@@ -15,9 +19,26 @@ public class Player : MonoBehaviour
         {
             shirt.sprite = blackShirt;
         }
-        else
+        if (index == 1)
         {
             shirt.sprite = whiteShirt;
         }
+        if (index == 2)
+        {
+            shirt.sprite = redShirt;
+        }
+        if (index == 3)
+        {
+            shirt.sprite = orangeShirt;
+        }
+        if (index == 4)
+        {
+            shirt.sprite = yellowShirt;
+        }
+        if (index == 5)
+        {
+            shirt.sprite = purpleShirt;
+        }
+
     }
 }
