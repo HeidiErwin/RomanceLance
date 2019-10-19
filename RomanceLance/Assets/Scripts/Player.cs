17a62ef90c0de59491ede81806cff05a6ed17546
sa-ge <sagematsushima@gmail.com>
2019-10-19 15:08:34 -0400

    Created tabs for the menu of items

--- RomanceLance/Assets/Scripts/Player.cs
+++ RomanceLance/Assets/Scripts/Player.cs
@@ -12,33 +12,50 @@ public class Player : MonoBehaviour
     [SerializeField] Sprite orangeShirt;
     [SerializeField] Sprite yellowShirt;
     [SerializeField] Sprite purpleShirt;
+    public Sprite currentShirt;
+    public Sprite currentLance;
+    public Sprite currentSteed;
 
     public void changeShirt(int index)
     {
-            if (index == 0)
+        if (index == 0)
         {
             shirt.sprite = blackShirt;
+            currentShirt = shirt.sprite;
         }
         if (index == 1)
         {
             shirt.sprite = whiteShirt;
+            currentShirt = shirt.sprite;
         }
         if (index == 2)
         {
             shirt.sprite = redShirt;
+            currentShirt = shirt.sprite;
         }
         if (index == 3)
         {
             shirt.sprite = orangeShirt;
+            currentShirt = shirt.sprite;
         }
         if (index == 4)
         {
             shirt.sprite = yellowShirt;
+            currentShirt = shirt.sprite;
         }
         if (index == 5)
         {
             shirt.sprite = purpleShirt;
+            currentShirt = shirt.sprite;
         }
+
+    }
+    public void changeLance(int index)
+    {
+
+    }
+    public void changeSteed(int index)
+    {
 
     }
 }
---
+++ RomanceLance/Assets/Scripts/TabManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TabManager : MonoBehaviour
+{
+    [SerializeField] int shirtButton;
+    [SerializeField] int lanceButton;
+    [SerializeField] int steedButton;
+    public static int tabSelected;
+
+    public void shirtTabs()
+    {
+        for (int i = 1; i < 7; i++) {
+            this.transform.GetChild(0).transform.GetChild(i).gameObject.SetActive(false);
+            this.transform.GetChild(1).transform.GetChild(i).gameObject.SetActive(false);
+            this.transform.GetChild(2).transform.GetChild(i).gameObject.SetActive(true);
+        }
+    }
+    public void lanceTabs()
+    {
+        for (int j = 1; j < 7; j++)
+        {
+            this.transform.GetChild(0).transform.GetChild(j).gameObject.SetActive(true);
+            this.transform.GetChild(1).transform.GetChild(j).gameObject.SetActive(false);
+            this.transform.GetChild(2).transform.GetChild(j).gameObject.SetActive(false);
+        }
+    }
+    public void steedTabs()
+    {
+        for (int k = 1; k < 7; k++)
+        {
+            this.transform.GetChild(0).transform.GetChild(k).gameObject.SetActive(false);
+            this.transform.GetChild(1).transform.GetChild(k).gameObject.SetActive(true);
+            this.transform.GetChild(2).transform.GetChild(k).gameObject.SetActive(false);
+        }
+    }
+
+}
